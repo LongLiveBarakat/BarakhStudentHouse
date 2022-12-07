f8cd3a68561f5cba0e2e5deb5208db0ea4f78fa2
LongLiveBarakat <114806148+LongLiveBarakat@users.noreply.github.com>
2022-12-07 23:50:43 +0200

    Add files via upload

---
+++ Student Housing /js/main.js
@@ -0,0 +1,307 @@
+// start declaring variablse //
+let fullcontainer = document.getElementById("Fullcontainer");
+
+let cardRooms = document.getElementById("containerRooms");
+
+let exit = document.getElementById("exit");
+
+let cardOne = document.getElementById("one");
+let cardTwo = document.getElementById("two");
+let cardThree = document.getElementById("three");
+let cardFour = document.getElementById("four");
+let cardFive = document.getElementById("five");
+let cardSix = document.getElementById("six");
+let cardSeven = document.getElementById("seven");
+let cardEight = document.getElementById("eight");
+let cardNine = document.getElementById("nine");
+let cardTen = document.getElementById("ten");
+let cardEleven = document.getElementById("eleven");
+let cardTwilve = document.getElementById("twilve");
+let cardThirteen = document.getElementById("thirteen");
+let cardFourteen = document.getElementById("fourteen");
+let cardFifteen = document.getElementById("fifteen");
+
+// end declaring variablse //
+
+// start functions //
+
+cardOne.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardOne.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardOne.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardOne.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardOne.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 1";
+})
+
+cardTwo.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardTwo.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardTwo.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardTwo.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardTwo.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 2";
+})
+
+cardThree.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardThree.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardThree.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardThree.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardThree.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 3";
+})
+
+cardFour.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardFour.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardFour.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardFour.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardFour.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 4";
+})
+
+cardFive.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardFive.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardFive.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardFive.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardFive.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 5";
+})
+
+cardSix.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardSix.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardSix.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardSix.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardSix.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 6";
+})
+
+cardSeven.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardSeven.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardSeven.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardSeven.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardSeven.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 7";
+})
+
+cardEight.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardEight.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardEight.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardEight.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardEight.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 8";
+})
+
+cardNine.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardNine.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardNine.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardNine.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardNine.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 9";
+})
+
+cardTen.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardTen.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardTen.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardTen.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardTen.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 10";
+})
+
+cardEleven.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardEleven.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardEleven.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardEleven.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardEleven.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 11";
+})
+
+cardTwilve.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardTwilve.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardTwilve.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardTwilve.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardTwilve.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 12";
+})
+
+cardThirteen.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardThirteen.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardThirteen.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardThirteen.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardThirteen.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 13";
+})
+
+cardFourteen.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardFourteen.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardFourteen.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardFourteen.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardFourteen.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 14";
+})
+
+cardFifteen.addEventListener("click" , function () {
+    fullcontainer.style.opacity = 0.3;
+})
+cardFifteen.addEventListener("click" , function () {
+    cardRooms.style.display = "block";
+})
+cardFifteen.addEventListener("click" , function () {
+    cardRooms.style.visibility = "visible";
+})
+cardFifteen.addEventListener("click" , function () {
+    cardRooms.style.zIndex = 9999;
+})
+cardFifteen.addEventListener("click" , function () {
+    const h2 = document.querySelector("#myh2");
+    h2.innerText = "Room 15";
+})
+
+document.getElementById("myButton").onclick = function () {
+    var myName = document.getElementById("myNameOne").value;
+    
+}
+
+// end functions //
+
+// start exit button //
+exit.addEventListener("click", function () {
+    cardRooms.style.display = "none";
+})
+
+exit.addEventListener("click", function () {
+    cardRooms.style.visibility = "hidden";
+})
+exit.addEventListener("click", function () {
+    fullcontainer.style.opacity = 1;
+})
+exit.addEventListener("click", function () {
+    
+})
+exit.addEventListener("click", function () {
+    
+})
+// end exit button //
